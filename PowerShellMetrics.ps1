98be713a9e0b42ad9085e880a0371ec43346c49c
walonsor <walteralonso20@yahoo.com>
2020-04-10 21:57:24 -0500

    upload power shell

---
+++ PowerShellMetrics.ps1
@@ -0,0 +1,10 @@
+﻿
+$xslt = New-Object System.Xml.Xsl.XslCompiledTransform;
+$xslt.Load("C:\Users\kokor\source\repos\Metrics\NetFramework\metrics.xslt");
+$arrayMetrics = Get-ChildItem -Include MetricsOutput.xml -Recurse
+Foreach ($i in $arrayMetrics){
+    $fragments = $i.ToString().split('\')
+    $nameAssembly = $fragments[ @($fragments).length - 2]
+    $xslt.Transform($i, "C:\Users\kokor\source\repos\Metrics\HTMLMetrics\"+$nameAssembly+".html");
+    Write-Host "generated" $nameAssembly;
+}
