1ee94b60a92d0b4f89bc7d5f9b4d3f3ed3709252
walonsor <walteralonso20@yahoo.com>
2020-04-10 21:18:53 -0500

    adding net core porject

---
+++ NetCoreApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace NetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for (int i = 0; i < 100; i++)
+            {
+                for (int j = 0; j < 100; j++)
+                {
+                    Console.WriteLine($"{i}:{j}");
+                }
+            }
+        }
+    }
+}
